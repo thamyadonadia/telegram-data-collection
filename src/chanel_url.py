54a197e608ad6cb2c923002790d9e4e5568f8118
thamyadonadia <thamya.donadia@edu.ufes.br>
2022-11-14 20:18:06 -0300

    started to collect images

---
+++ src/chanel_url.py
@@ -0,0 +1,8 @@
+from urllib.parse import urlparse, parse_qs
+
+def url_treatment(url):
+    url_treated = urlparse(url)
+    parameters = parse_qs(url_treated.fragment)
+    chanel_id = str(parameters.get('/im?p')).strip("'[]").split("_")
+    chanel_id = chanel_id[0].replace('c', '-100')
+    return int(chanel_id)
--- start.py
+++ start.py
@@ -1,13 +1,33 @@
 import dotenv
 import os 
+import src.chanel_url as chanel_url
 
 from telethon import TelegramClient
+from telethon.tl.types import InputMessagesFilterPhotos
+from telethon.tl.types import PhotoSize
 
-# procurando e carregando .env na mesma pasta
+# searching and loading .env 
 dotenv.load_dotenv(dotenv.find_dotenv())
 
 api_id = os.getenv("api_id")
 api_hash = os.getenv("api_hash")
 
-with TelegramClient('anon', api_id, api_hash) as client:
-    client.loop.run_until_complete(client.send_message('@AfonsoSalvador', 'oiiiiiiiiiiiiiiiiii'))
+session_name = input("Enter the name of the ession: ")
+client = TelegramClient(session_name, api_id, api_hash)
+
+async def main():
+    url = input("Enter the URL of the chanel: ")
+    chanel_id = chanel_url.url_treatment(url)
+    file_path = "./images"
+    
+    #TODO: colocar um .txt com os links pra ele ler e coletar de cada canal no .txt
+    #TODO: mexer com venv e entender o PhotoSize
+    async for message in client.iter_messages(chanel_id, filter=InputMessagesFilterPhotos):
+       #await message.download_media(file_path, thumb = PhotoSize(type = "webp", w = 300, h = 300, size = 0))
+        await message.download_media(file_path)
+
+    # await client.log_out
+
+with client:
+    client.loop.run_until_complete(main())
+    
