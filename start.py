b1598842647d28f0583456c3f57df9aa3e73a655
thamyadonadia <thamya.donadia@edu.ufes.br>
2023-05-01 18:01:34 -0300

    :sparkles: added hash comparison

--- start.py
+++ start.py
@@ -1,10 +1,9 @@
-import dotenv
-import os 
+import dotenv, os 
 import src.channels as channels
-import src.files as files
+import src.setup as setup
+import src.hash as hash
 
 from telethon import TelegramClient
-import telethon.utils as utils
 
 # searching and loading .env 
 dotenv.load_dotenv(dotenv.find_dotenv())
@@ -12,15 +11,15 @@ dotenv.load_dotenv(dotenv.find_dotenv())
 api_id = os.getenv("api_id")
 api_hash = os.getenv("api_hash")
 
-#session_name = input("Enter the name of the session: ")
 session_name = "20jan"
 client = TelegramClient(session_name, api_id, api_hash)
 
 async def main():
     # get the file that contains the chanel's ou group's ids
-    # url_file = input("Enter the name of the file that contains the IDs: ")
-    url_file = "channels.csv"
-    channel_dict = channels.read_chanels(url_file)
+    channels_file = "./in/channels.csv"
+    channel_dict = channels.read_chanels(channels_file)
+    hash_set = hash.hash_start("./docs/hash.csv")
+    hash_file = open("./docs/hash.csv", "a")
     
     # creates a directory to store the media from channels and groups  
     if(not os.path.exists("./media/")):
@@ -29,7 +28,7 @@ async def main():
     for x in channel_dict:
         channel_id = channel_dict[x]
         media_path = "./media/" + str(channel_id)
-        
+                
         # creates a directory with the name of the chanel to store the media 
         if(not os.path.exists(media_path)):
             os.mkdir(media_path)
@@ -37,10 +36,16 @@ async def main():
         async for message in client.iter_messages(channel_id):
             
             if (message.photo or message.video or message.voice or message.audio) and (not message.web_preview): 
-                media_path = "./media/" + str(channel_id)
-                media_path = files.organize_files(media_path, message)
-                path = await message.download_media(media_path)
-                files.rename_files(message, path, media_path)
+                
+                if hash.new_media(hash_set, message):
+                    media_path = "./media/" + str(channel_id)
+                    media_path = setup.organize_files(media_path, message)
+                    path = await message.download_media(media_path)
+                    dst = setup.rename_files(message, path, media_path)
+                    hash.write_hash(hash_file, message, dst)
+                    hash_set = hash.update_set(hash_set, message)               
+    
+    hash_file.close()
 
 with client:
     client.loop.run_until_complete(main())
