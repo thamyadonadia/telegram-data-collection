778f99b57517908aa8d42e9aca3405b46ef148df
thamyadonadia <thamya.donadia@edu.ufes.br>
2023-08-19 16:39:33 -0300

    :fire: removed some files

--- src/channels.py
+++
@@ -1,8 +0,0 @@
-import csv
-
-def read_chanels(file_name):
-    with open(file=file_name, mode = "r") as file:
-        reader = csv.reader(file)
-        channel_dict = {rows[0]:int(rows[1]) for rows in reader}
-    return channel_dict
-
